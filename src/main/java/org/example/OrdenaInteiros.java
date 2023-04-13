a70342f0c7de22c344eee4a493d5773fd6a8df35
Marcilio Zanatta <marcsilva@brasilseg.com.br>
2023-04-12 21:22:58 -0300

    Ordena Inteiro

---
+++ src/main/java/org/example/OrdenaInteiros.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class OrdenaInteiros implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (o1 < o2) return -1;
+        if (o1 > o2) return 1;
+        return 0;
+    }
+}
