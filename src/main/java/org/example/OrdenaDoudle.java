8863923338121a75802c96b1114e3067da009caa
Marcilio Zanatta <marcsilva@brasilseg.com.br>
2023-04-12 21:22:46 -0300

    Ordena double

---
+++ src/main/java/org/example/OrdenaDoudle.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class OrdenaDoudle implements Comparator<Double> {
+    @Override
+    public int compare(Double o1, Double o2) {
+        if (o1 < o2) return -1;
+        if (o1 > o2) return 1;
+        return 0;
+    }
+}
