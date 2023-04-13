be8024c14082157a974cd72def59a9dfb3b1479f
Marcilio Zanatta <marcsilva@brasilseg.com.br>
2023-04-12 21:18:50 -0300

    Up

--- src/main/java/org/example/OrdenaStrings.java
+++ src/main/java/org/example/OrdenaStrings.java
@@ -61,6 +61,7 @@ class  ComparadorPorTamanho implements Comparator<String>{
 
     @Override
     public int compare(String s1, String s2) {
+
         if(s1.length() < s2.length()) return -1;
         if(s1.length() > s2.length()) return 1;
         return 0;
